4c292792f20adaa736acc289ef573255d2fe8a5d
Jeison Iriarte <jeison.18.efra@gmail.com>
2021-03-10 02:58:09 -0500

    Actualización envío de contacto

--- contact.php
+++ contact.php
@@ -1,68 +1,28 @@
 <?php
-        if (!empty($_SERVER['HTTP_X_REQUESTED_WITH'])){
-          // Recibir vía POST los datos del formulario
-          $name = $_POST["name"];
-          $email = $_POST["email"];
-          $subject = $_POST["subject"];
-          $message = $_POST["message"];
+	if (isset($_POST['fname'])){
+		$nombres=htmlentities($_POST['fname']);
+		$email_cliente=htmlentities($_POST['email']);
+		$subject=utf8_decode($_POST['subject']);
+		$mensaje=htmlentities($_POST['message']);
 
-          if (empty($correo)){ // Validar si la dirección de correo no esta vacia
-            $error=1;
-            $mensaje="Please complete all the fields of the form.";
-            $datos=0;
-          } else {
+	/*SIGUE RECOLECTANDO DATOS PARA FUNCION MAIL*/
+	$message = '';
+	$message .= '<p>Hello, a new message has been registered from the contact form of the website, according to the following detail:</p> ';
+	$message .= '<p>Client: '.$nombres.'</p> ';
+	$message .= '<p>Email: '.$email_cliente.'</p> ';
+	$message .= '<p>Subjec: '.$subject.'</p> ';
+	$message .= '<p>Message: '.$mensaje.'</p> ';
 
-            $usuario_mail="info@micomcommodities.com"; // Direccion de envio
-            $remite = "www.micomcommodities.com"; //Nombre de Quien remite o envia
-            $remite_email = "no-reply@micomcommodities.com";
-            $asunto = "Contact email from $remite";
+	$header = "MIME-Version: 1.0\r\n";
+	$header .= "Content-type: text/html; charset=UTF-8\r\n";
+	$header .= "From: ". $nombres . " <" . $email_cliente . ">\r\n";
+	$email='info@micomcommodities.com';//Ingresa tu dirección de correo
 
-            // Armar un mensaje html para el cuerpo del correo electrónico
-            $message = "<!doctype html>
-            <html class=''><head><meta charset='utf-8'>
-            <title>They have sent the following contact form</title>
-            </head>
-            <body>
-            <h1>Contact from www.micomcommodities.com <br clear='all'/>(online)</h1><br clear='all'/>
-            Name: ".$name." <br clear='all'/>
-            Email: ".$email." <br clear='all'/>
-            Subject: ".$subject." <br clear='all'/>
-            Message: <br clear='all'/> ".$message." <br clear='all'/>
-            </body></html>";
-
-            $cabeceras = "From: ".$remite." <".$remite_email.">\r\n";
-            $cabeceras = $cabeceras."Mime-Version: 1.0\n";
-            $cabeceras = $cabeceras.'Content-type: text/html; charset=utf-8' . "\r\n";
-
-            // Realizar el envío con la función mail de php
-            $enviar_email = mail($usuario_mail, $asunto, $mensaje, $cabeceras);
-
-            if($enviar_email) { // Envío exitoso
-              $error=0;
-              $mensaje="Mail sent, we will be answering you as soon as possible.";
-              $datos=0;
-            }else { // No se pudo enviar el correo
-              $error=1;
-              $mensaje="The mail could not be sent, please try again.";
-              $datos=0;
-            }
-          }
-
-        // Empaquetado de la respuesta en formato JSON
-          $resp=[
-            "error"=>$error,
-            "mensaje"=>$mensaje,
-            "datos"=>$datos,
-          ];
-
-        echo json_encode($resp);
-
-        } else {
-          $resp=[
-           "error"=>1,
-           "mensaje"=>"The server denied the request.",
-           "datos"=>0
-          ];
-          echo json_encode($resp);
-        }
-        ?>
+	if (mail($email,$subject,$message,$header)){
+		echo 'success';
+	}	 else {
+		echo 'The message could not be sent.';
+	}
+	/*FINALIZA RECOLECTANDO DATOS PARA FUNCION MAIL*/
+	}
+?>
--- js/contact.js
+++ js/contact.js
@@ -1,43 +1,66 @@
-(function enviar_correo() {
+document.addEventListener("touchstart", function() {}, false);
+(function($) {
     "use strict";
+    $(function() {
+        var randNumber_1 = parseInt(Math.ceil(Math.random() * 15), 10);
+        var randNumber_2 = parseInt(Math.ceil(Math.random() * 15), 10);
+        humanCheckCaptcha(randNumber_1, randNumber_2);
+    });
 
-    jQuery(document).ready(function ($) {
-        $(document).on('submit', '#main_contact_form', function (e) {
-            e.preventDefault();
-            var name = $('#name').val();
-            var email = $('#email').val();
-            var subject = $('#subject').val();
-            var message = $('#message').val();
+    function humanCheckCaptcha(randNumber_1, randNumber_2) {
+        $("#humanCheckCaptchaBox").html("Solve The Math ");
+        $("#firstDigit").html('<input name="mathfirstnum" id="mathfirstnum" class="form-control" type="text" value="' + randNumber_1 + '" readonly>');
+        $("#secondDigit").html('<input name="mathsecondnum" id="mathsecondnum" class="form-control" type="text" value="' + randNumber_2 + '" readonly>');
+    }
+    $("#contactForm").validator().on("submit", function(event) {
+		var parametros =$(this).serialize();
+        if (event.isDefaultPrevented()) {
+            formError();
+            submitContactFormActionMSG(false, "Please complete the form correctly");
+        } else {
+            var fname = $("#fname").val();
+            var email = $("#email").val();
+            var subject = $("#subject").val();
+            var message = $("#message").val();
+            var filter = /^((\+[1-9]{1,4}[ \-]*)|(\([0-9]{2,3}\)[ \-]*)|([0-9]{2,4})[ \-]*)*?[0-9]{3,4}?[ \-]*[0-9]{3,4}?$/;
 
-            if (name && email && subject && message) {
-                $.ajax({
+            event.preventDefault();
+            $.ajax({
                     type: "POST",
-                    url: '../contact.php',
-                    data: {
-                        'name': name,
-                        'email': email,
-                        'subject': subject,
-                        'message': message,
-                    },
-                    success: function (data) {
-                        $('#contact_form_submit').children('.email-success').remove();
-                        $('#contact_form_submit').prepend('' + data + '');
-                        $('#name').val('');
-                        $('#email').val('');
-                        $('#message').val('');
-                        $('.email-success').fadeOut(3000);
-                    },
-                    error: function (res) {
-
+                    url: "contact.php",
+                    data: parametros,
+                    success: function(text) {
+                        if (text == "success") {
+                            contactFormSuccess();
+                        } else {
+                            formError();
+                            submitContactFormActionMSG(false, text);
+                        }
                     }
                 });
-            } else {
-                $('#contact_form_submit').children('.email-success').remove();
-                $('#contact_form_submit').prepend('');
-                $('.email-success').fadeOut(3000);
-            }
+        }
+    });
 
-        });
-    })
+    function submitContactFormActionMSG(valid, msg) {
+        if (valid) {
+            var msgClasses = "h3 text-center text-success col-md-12";
+        } else {
+            var msgClasses = "h3 text-center text-danger col-md-12";
+        }
+        $("#msgContactSubmit").removeClass().addClass(msgClasses).text(msg);
+        return false;
+    }
 
-}(jQuery));
+    function contactFormSuccess() {
+        submitContactFormActionMSG(true, "The data has been sent successfully");
+    }
+
+
+   
+
+    
+
+    function formError() {
+        $(".help-block.with-errors").removeClass('hidden');
+    }
+})(jQuery);
